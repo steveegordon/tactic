e234b5c91c120ce4b40a2a7f048e421d209c4c12
stevegordon <tdtsgordon@gmail.com>
2017-04-15 15:08:14 -0700

    handler/view logic add placeholder css

--- javascripts/app.js
+++ javascripts/app.js
@@ -20,7 +20,6 @@ var game = {
     });
   },
   changeBox: function(number){
-    // debugger;
     var square = this.board[number];
     var playerTurn = this.turn;
     if (square.user === 0){
@@ -40,53 +39,118 @@ var game = {
     var square = this.board;
     if (square[0].user === square[1].user && square[2].user === square[0].user && square[0].user !== 0){
       console.log(player + " wins!");
-      this.resetGame();}
+      handlers.resetGame();}
     else if (square[3].user === square[4].user && square[4].user === square[5].user && square[3].user !== 0){
       console.log(player + " wins!");
-      this.resetGame();}
+      handlers.resetGame();}
     else if (square[6].user === square[7].user && square[7].user === square[8].user && square[6].user !== 0){
       console.log(player + " wins!");
-      this.resetGame();}
+      handlers.resetGame();}
     else if (square[0].user === square[3].user && square[6].user === square[0].user && square[0].user !== 0){
       console.log(player + " wins!");
-      this.resetGame();}
+      handlers.resetGame();}
     else if (square[1].user === square[4].user && square[7].user === square[1].user && square[1].user !== 0){
       console.log(player + " wins!");
-      this.resetGame();}
+      handlers.resetGame();}
     else if (square[2].user === square[5].user && square[8].user === square[2].user && square[2].user !== 0){
       console.log(player + " wins!");
-      this.resetGame();}
+      handlers.resetGame();}
     else if (square[0].user === square[4].user && square[8].user === square[0].user && square[0].user !== 0){
       console.log(player + " wins!");
-      this.resetGame();}
+      handlers.resetGame();}
     else if (square[6].user === square[4].user && square[2].user === square[6].user && square[6].user !== 0){
       console.log(player + " wins!");
-      this.resetGame();}
+      handlers.resetGame();}
     else if (this.turn === 9){confirm("Cats Game. Play again?");
-      this.resetGame();}
+      handlers.resetGame();}
     else {return;}
   }
 };
 
 var handlers = {
   // Change player name
-  changePlayerName: function(){
+  changePlayerName: function(player, name){
 
   },
   // Change game name
-  changeGameName: function(){
+  changeGameName: function(name){
 
   },
   // New game
-  newGame: function(){
-
+  startGame: function(){
+    game.createGame();
+    view.displayGame();
+    this.boardClicks();
   },
   // Reset game
   resetGame: function(){
-
+    game.resetGame();
+    view.resetBoxes();
   },
   // Take turn
-  takeTurn: function(){
-    
+  takeTurn: function(id, square){
+    if (square.classList.length === 1){
+      view.changeBox(square);
+      game.changeBox(id);
+    }
+  },
+  boardClicks: function(){
+    var area = document.getElementById('board');
+    area.addEventListener('click', function(event){
+      var square = event.target;
+      handlers.takeTurn(square.id, square);
+    });
+  }
+};
+
+var view = {
+  displayGame: function(){
+    var container = document.getElementById('container');
+    var startGameButton = document.getElementById('startGameButton');
+    var header = document.querySelector('header');
+    var title = document.createElement('h1');
+    title.textContent = "New Game";
+    var player1Data = document.createElement('div');
+    player1Data.className = 'playerData';
+    var player2Data = document.createElement('div');
+    player2Data.className = 'playerData';
+    var board = document.createElement('div');
+    board.id = 'board';
+    game.board.forEach(function(square, index){
+      var box = document.createElement('div');
+      box.id = index;
+      box.className = 'square';
+      board.appendChild(box);
+    });
+    container.removeChild(startGameButton);
+    header.appendChild(title);
+    container.appendChild(player1Data);
+    container.appendChild(board);
+    container.appendChild(player2Data);
+    this.createSettingsButton(header);
+  },
+  changeBox: function(square){
+    var box = game.board;
+    if (game.turn === 1){
+      square.classList.add('p1');
+    }
+    else {
+      square.classList.add('p2');
+    }
+  },
+  resetBoxes: function(){
+    var boxes = document.querySelectorAll('div.square');
+    boxes.forEach(function(box){
+      box.classList.remove('p1', 'p2');
+    });
+  },
+  createSettingsButton: function(header){
+    var settingsButton = document.createElement('button');
+    settingsButton.className = 'settings';
+    settingsButton.onclick = function(){view.toggleOverlay();};
+    header.appendChild(settingsButton);
+  },
+  toggleOverlay: function(){
+   document.body.classList.toggle('overlay');
   }
 };
