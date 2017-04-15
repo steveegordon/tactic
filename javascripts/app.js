864c854ccb6b4e0e133a71eb158b53d70e534e60
stevegordon <tdtsgordon@gmail.com>
2017-04-15 09:53:05 -0700

    added game methods

---
+++ javascripts/app.js
@@ -0,0 +1,64 @@
+var game = {
+  board: [],
+  turn: 1,
+  createGame: function(){
+    for (var i = 0; i < 9; i++){
+      this.board.push({
+        square: i,
+        user: 0
+      });
+    }
+  },
+  resetGame: function(){
+    this.board.forEach(function(box){
+      box.user = 0;
+    });
+  },
+  changeBox: function(number){
+    // debugger;
+    var square = this.board[number];
+    var playerTurn = this.turn;
+    if (square.user === 0){
+      if (playerTurn === 1){
+        square.user = 1;
+        this.turn = 2;
+        this.checkWin(playerTurn);
+      }
+      else {
+        square.user = 2;
+        this.turn = 1;
+        this.checkWin(playerTurn);
+      }
+    }
+  },
+  checkWin: function(player){
+    var square = this.board;
+    if (square[0].user === square[1].user && square[2].user === square[0].user && square[0].user !== 0){
+      console.log(player + " wins!");
+      this.resetGame();}
+    else if (square[3].user === square[4].user && square[4].user === square[5].user && square[3].user !== 0){
+      console.log(player + " wins!");
+      this.resetGame();}
+    else if (square[6].user === square[7].user && square[7].user === square[8].user && square[6].user !== 0){
+      console.log(player + " wins!");
+      this.resetGame();}
+    else if (square[0].user === square[3].user && square[6].user === square[0].user && square[0].user !== 0){
+      console.log(player + " wins!");
+      this.resetGame();}
+    else if (square[1].user === square[4].user && square[7].user === square[1].user && square[1].user !== 0){
+      console.log(player + " wins!");
+      this.resetGame();}
+    else if (square[2].user === square[5].user && square[8].user === square[2].user && square[2].user !== 0){
+      console.log(player + " wins!");
+      this.resetGame();}
+    else if (square[0].user === square[4].user && square[8].user === square[0].user && square[0].user !== 0){
+      console.log(player + " wins!");
+      this.resetGame();}
+    else if (square[6].user === square[4].user && square[2].user === square[6].user && square[6].user !== 0){
+      console.log(player + " wins!");
+      this.resetGame();}
+    else if (this.turn === 9){confirm("Cats Game. Play again?");
+      this.resetGame();}
+    else {return;}
+  }
+};
